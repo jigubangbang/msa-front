ccc664b426a562e275638711be5558dd7071f52e
nlhyll <pnhmms@gmail.com>
2025-06-24 12:45:59 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import AppRouter from "./routes/AppRouter";
 import Header from "./components/main/Header";
 
-
 function App() {
   return (
     <div className="app-container">
