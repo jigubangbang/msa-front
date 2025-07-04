8e08e15041c6a89843858a128b5bf2a1e68c5173
seolyeong333 <33qqpp@naver.com>
2025-07-04 11:52:06 +0900

    add Init Theme:Dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import { ThemeContext } from "./utils/themeContext";
 import Header from "./components/main/Header";
 import ChatModal from "./pages/Chat/ChatModal";
 import './App.css';
@@ -16,22 +17,29 @@ function ScrollToTop() {
 }
 
 function App() {
-  const [isChatModal, setIsChatModal] = useState(false); // 채팅 모달 상태
+
+  // 다크모드 - useContext를 이용하여 prop 전달
+  const [isDark, setIsDark] = useState(false);
+  // 채팅
+  const [isChatModal, setIsChatModal] = useState(false);
   const openChatModal = () => setIsChatModal(true);
   const closeChatModal = () => setIsChatModal(false);
+
   return (
-    <div className="app-container">
-      <Header onOpenChat={openChatModal} ></Header>
-      <main className="main-container">
-      <ScrollToTop />
-      <AppRouter/>
-      <ChatModal
-        isOpen={isChatModal}
-        onClose={closeChatModal}
-        chatId={1} // 필요한 채팅방 ID
-      />
-      </main>
-    </div>
+    <ThemeContext.Provider value={{isDark, setIsDark}}>
+      <div className="app-container">
+        <Header onOpenChat={openChatModal} ></Header>
+        <main className="main-container">
+        <ScrollToTop />
+        <AppRouter/>
+        <ChatModal
+          isOpen={isChatModal}
+          onClose={closeChatModal}
+          chatId={1} // 필요한 채팅방 ID
+        />
+        </main>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
